437c7b56c3b295708a0ae8936867bf9efdefa362
ShaileshRimal <127722077+ShaileshRimal@users.noreply.github.com>
2023-03-13 18:08:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let particles = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 400);
   for (let i = 0; i < 100; i++) {
     particles.push(new Particle());
   }
